43d21dc1995c727bec9a36a69ed6b847defffa9c
Practicas Software 1 <PracticasSoftware1@proconsi.local>
2022-04-18 16:54:14 +0200

    Estructurando carpetas del proyecto. Primeras pruebas para verificar la correcta instalación

---
+++ src/components/Header/HeaderItem.vue
@@ -0,0 +1,25 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+
+export default {
+  name: "HeaderItem",
+  components: {
+      RouterLink,
+      RouterView
+  },
+  setup(props) {},
+};
+</script>
+
+<template>
+  <header>
+       <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">Otro</RouterLink>
+      </nav>
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "HomeView",
+    setup(props) {
+        
+    },
+}
+</script>
+
+<template>
+    <h1>Hello</h1>
+</template>
+
+<style scoped>
+
+</style>
