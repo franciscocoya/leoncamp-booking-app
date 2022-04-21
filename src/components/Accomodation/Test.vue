8f994fe609a5e8a15fc747b4ca267158ad729a42
Practicas Software 1 <PracticasSoftware1@proconsi.local>
2022-04-21 08:01:32 +0200

    Copia previa pull v1.0.0

---
+++ src/components/Accomodation/Test.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    const props = defineProps({
+        text: {
+            type: String,
+            required: true,
+        },
+        buttonStyle: {
+            type: String,
+            required: true,
+        },
+        buttonIcon: {
+            type: String,
+            required: true,
+        },
+        iconDirection: {
+            type: String,
+            required: false,
+        },
+    });
+</script>
+
+<template>
+    
+</template>
